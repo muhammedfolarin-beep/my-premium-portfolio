a47769b1b45b9e53b875e3d36603a7b271ac82cf
muhammedfolarin-beep <muhammed.folarin@gmail.com>
2026-07-17 00:53:06 +0100

    Explicitly set Next.js basePath for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: "export",
+  basePath: isProd ? '/my-premium-portfolio' : '',
   images: {
     unoptimized: true,
   },
